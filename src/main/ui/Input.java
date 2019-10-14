d94be6f2614503b2f6d4c9b89c486118567217bb
kevin <kevinfang87@gmail.com>
2019-10-13 18:12:58 -0700

    finished recursion call in exception, cleaned up input in any case. need to add exception for owe or owed

--- src/main/ui/Input.java
+++ src/main/ui/Input.java
@@ -31,15 +31,10 @@ public class Input {
         Scanner input = new Scanner(System.in);
         askLoadOrNew();
         while (true) {
-            askPersonOrUrgentPerson();
-            try {
-                debtsList.logResult(debt, amount, oweOrOwed, who, dueDate);
-            } catch (integerException e) {
-                System.out.println("You entered a negative or zero amount!\nPlease Enter your entry again.");
-            }
+            userInput();
             System.out.println("Are you finished imputing numbers? (Type Yes or No)");
             String done = input.next();
-            if (done.equals("Yes")) {
+            if (done.equalsIgnoreCase("Yes")) {
                 break; // referenced from B04 simple calculator lecture lab
             }
         }
@@ -47,13 +42,25 @@ public class Input {
         printList();
     }
 
+    public void userInput() {
+        askPersonOrUrgentPerson();
+        try {
+            debtsList.logResult(debt, amount, oweOrOwed, who, dueDate);
+        } catch (integerException e) {
+            System.out.println("You entered a negative or zero amount!\nPlease enter your entry again.");
+            userInput();
+        } finally {
+            System.out.println("Thank You.");
+        }
+
+    }
 
 
     private void askPersonOrUrgentPerson() {
         Scanner input = new Scanner(System.in);
         System.out.println("Would you like to create and urgent item or a regular item? (Type 'Urgent' or 'Regular')");
         String urgentOrRegular = input.next();
-        if (urgentOrRegular.equals("Regular")) {
+        if (urgentOrRegular.equalsIgnoreCase("Regular")) {
             normalDebt();
         } else {
             urgentDebt();
@@ -62,7 +69,7 @@ public class Input {
 
     public void normalDebt() {
         debt = new NormalDebt();
-        userInput(debt);
+        debtInput(debt);
         /*try {
             debtsList.logResult(debt, amount, oweOrOwed, who, "No due date");
         } catch (integerException e) {
@@ -76,7 +83,7 @@ public class Input {
         debt = new UrgentDebt();
         System.out.println("What is the date this debt is due?");
         dueDate = input.next();
-        userInput(debt);
+        debtInput(debt);
        /* try {
             debtsList.logResult(debt, amount, oweOrOwed, who, dueDate);
         } catch (integerException e) {
@@ -88,18 +95,18 @@ public class Input {
         Scanner input = new Scanner(System.in);
         System.out.println("Would you like to load a previous list or create a new list? (Type 'Load' or 'New')");
         String loadOrNew = input.next();
-        if (loadOrNew.equals("Load")) {
+        if (loadOrNew.equalsIgnoreCase("Load")) {
             debtsList.load();
         }
     }
 
     // REQUIRES: Person
     // EFFECTS: passes user input into logResult to be logged
-    public void userInput(Debt person) {
+    public void debtInput(Debt person) {
         Scanner input = new Scanner(System.in);
         System.out.println("Do you owe money or are you owed money? (Type Owe or Owed)");
         oweOrOwed = input.next();
-        if (oweOrOwed.equals("Owed")) {
+        if (oweOrOwed.equalsIgnoreCase("Owed")) {
             System.out.println("Please enter the amount owed to you");
             amount = input.nextInt();
             System.out.println("Who owes you this money?");
