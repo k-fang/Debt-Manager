8d08aba01f8fdcb5f3e71648f92a5b78708340ec
ankykf <58371107+ankykf@users.noreply.github.com>
2020-01-24 17:05:01 -0800

    Update Input.java

--- src/main/ui/Input.java
+++ src/main/ui/Input.java
@@ -112,8 +112,11 @@ public class Input implements ActionListener {
         enterClicked = false;
         value = false;
         loop = false;
+        try { 
         recurringDebtsList.load();
         normalUrgentDebtsList.load();
+        } catch (FileNotFoundException e) {
+        }
 
         run();
     }
