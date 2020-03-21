83806be889e87fb6f0266ab034aa187742633cd8
ankykf <58371107+ankykf@users.noreply.github.com>
2020-03-20 21:24:25 -0700

    Update Input.java
    fixed exception thrown on certain computers

--- src/main/ui/Input.java
+++ src/main/ui/Input.java
@@ -8,6 +8,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class Input implements ActionListener {
     private NormalUrgentDebtsList normalUrgentDebtsList;
